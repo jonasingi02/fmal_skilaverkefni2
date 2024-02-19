eb6853ebd168e8f5bd89a0c5f8ac53c7afe2c4de
jonasingi02 <124891230+jonasingi02@users.noreply.github.com>
2024-02-19 19:10:09 +0000

    /a[ sem 'eg kominn me[

---
+++ eparser.py
@@ -0,0 +1,104 @@
+class intermediateCodeGenerator():
+
+    def __init__(self):
+        self.interCode = []
+
+    def main(self, code):
+        self.tokens = code.split()
+        self.curr_token_nr = 0
+        self.eparser()
+
+    def get_next_token(self):
+        if self.curr_token_nr < len(self.tokens):
+            token = self.tokens[self.curr_token_nr]
+            self.curr_token_nr += 1
+            self.next_token = token
+            return token
+        return None
+
+    def validate_token(self, expected_token):
+        if expected_token == 'id' and self.next_token.isidentifier() or self.next_token == expected_token or expected_token == 'int' and isinteger(self.next_token):
+            return True
+        return False
+    
+
+        
+    def eparser(self):
+        self.parse()
+        self.get_next_token()
+        while(self.next_token == ";"):
+            self.parse()
+        self.validate_token("end")
+
+    def parse(self):
+        self.get_next_token()
+        if self.validate_token('print'):
+            self.get_next_token()
+            var = self.validate_token('id')
+            if (var):
+                var = self.next_token
+            self.interCode.append(f'PUSH {var}')
+            self.interCode.append('PRINT')
+        elif self.validate_token("id"):
+            if self.next_token == 'end':
+                pass
+            else:
+                var = self.next_token
+                self.get_next_token()
+                self.validate_token('=')
+                self.get_next_token()
+                self.expr()
+                self.interCode.append(f'ASSIGN')
+                self.interCode.append(f'PUSH {var}')
+        else:
+            raise SyntaxError(f'Unexpected token: {self.next_token}')
+
+    def expr(self):
+        self.term()
+        while self.next_token in ('+', '-'):
+            op = self.next_token
+            self.get_next_token()
+            self.term()
+            if op == '+':
+                self.interCode.append('ADD')
+            elif op == '-':
+                self.interCode.append('SUB')
+
+    def term(self):
+        self.factor()
+        while self.next_token == '*':
+            self.get_next_token()
+            self.factor()
+            self.interCode.append('MULT')
+
+    def factor(self):
+        if self.validate_token('int'):
+            value = self.next_token
+            self.interCode.append(f'PUSH {value}')
+            self.get_next_token()
+        elif self.next_token == 'id':
+            var = self.next_token
+            self.interCode.append(f'PUSH {var}')
+            self.get_next_token()
+        elif self.next_token == '(':
+            self.get_next_token()
+            self.expr()
+            self.validate_token(')')
+            self.get_next_token()
+        else:
+            raise SyntaxError(f'Unexpected token: {self.next_token}')
+        
+
+def isinteger(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+        
+input_str = "print x ; y = 5 + ( 3 * 2 ) ; end"
+generator = intermediateCodeGenerator()
+generator.main(input_str)
+
+for code_line in generator.interCode:
+    print(code_line)
---
+++ test_elexer.py
@@ -0,0 +1,10 @@
+from etoken import EToken
+from elexer import ELexer
+
+if __name__ == "__main__":
+    lexer = ELexer()
+    curr_token = EToken("",EToken.ERROR)
+
+    while curr_token.token_code != EToken.END:
+        curr_token = lexer.get_next_token()
+        print(curr_token)
