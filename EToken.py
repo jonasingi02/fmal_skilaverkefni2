dba733ee76f92afcd67d18585aca2a664a465c8a
Dagurko <112619676+Dagurko@users.noreply.github.com>
2024-02-19 22:34:22 +0100

    Token code

--- EToken.py
+++ EToken.py
@@ -1,4 +1,19 @@
 class EToken:
+    ID = 0
+    ASSIGN = 1
+    SEMICOL = 2
+    INT = 3
+    PLUS = 4
+    MINUS = 5
+    MULT = 6
+    LPAREN = 7
+    RPAREN = 8
+    PRINT = 9
+    END = 10
+    ERROR = 11
+    
     def __init__(self, lexeme, token_type):
         self.lexeme = lexeme
         self.token_type = token_type
+
+
